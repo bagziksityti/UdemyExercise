ee8f210ff9290447c8cd001abf6594f9df4d5f35
mariogithub <bagyinszki.mario1@gmail.com>
2025-11-15 01:04:12 +0000

    prefab

--- Assets/Scripts/triggerProject.cs
+++ Assets/Scripts/triggerProject.cs
@@ -3,14 +3,35 @@ using UnityEngine;
 public class triggerProject : MonoBehaviour
 {
 
-    [SerializeField] GameObject projectile;
+    [SerializeField] GameObject[] projectile;
+    void Start()
+    {
+        projectile = new GameObject[4];
+        for (int i = 0; i < projectile.Length; i++)
+        {
+            projectile[i] = new GameObject;
+
+        }
+    }
     private void OnTriggerEnter(Collider other)
     {
-       
+
 
         if (other.gameObject.tag == ("Player"))
+
         {
-            projectile.SetActive(true);
+            for (int i = 0; i < projectile.Length; i++)
+            {
+                if (projectile[i] != null)
+                {
+                    projectile[i].SetActive(true);
+                }
+                if (projectile[i].transform.position == other.transform.position)
+                {
+                    Destroy(projectile[i]);
+                }
+
+            }
         }
     }
 }
